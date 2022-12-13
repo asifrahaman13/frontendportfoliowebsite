e3ae63da43693ea9acb53421b6354c0ad6e02081
asifrahaman13 <asifrahaman162@gmail.com>
2022-12-13 16:10:57 +0530

    another commit

--- src/App.js
+++ src/App.js
@@ -13,10 +13,10 @@ const App = () => {
     <>
       <>
         <body>
-          <BrowserRouter>
+          <BrowserRouter basename={process.env.PUBLIC_URL}>
             <Navbar />
             <Routes>
-              <Route path="*" element={<Home />} />
+              <Route path="/" element={<Home />} />
               <Route path="/about" element={<About />} />
               <Route path="/contact" element={<Contact />} />
             </Routes>
