92debac97ade8b10a56ef38d7348b07842b60a22
asifrahaman13 <asifrahaman162@gmail.com>
2022-12-13 15:54:49 +0530

    another commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ const App = () => {
           <BrowserRouter>
             <Navbar />
             <Routes>
-              <Route path="/" element={<Home />} />
+              <Route path="/frontendportfoliowebsite" element={<Home />} />
               <Route path="/about" element={<About />} />
               <Route path="/contact" element={<Contact />} />
             </Routes>
